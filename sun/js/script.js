8c5dbd55e8ecb97342c888458441d713212ecf68
Liber <stinger87@yandex.ru>
2017-09-01 15:40:40 +0300

    Add files via upload

--- sun/js/script.js
+++ sun/js/script.js
@@ -1,59 +1,30 @@
-	let $slider = $('#slider');
-	$slider.find('div').each(function () {
-		$(this).data('number', $(this).parent().attr('class'));
-		if ($(this).attr('class') === 'day') {
-			$(this).css({'background': 'url(images/background_' + $(this).data('number') + '.jpg) no-repeat center'})
-		}
-		else {
-			$(this).css({'background': 'url(images/background_' + $(this).data('number') + '_night.jpg) no-repeat center'})
-		}
-	});
 
-	$slider.children(':first').addClass('active');
-	$slider.children().not(':first').hide();
-	setInterval("change()", 10000);
+$('#slider li:first').addClass('active');
+$('.day').addClass('active');
+setInterval("change()",10000);
+let index=0;
+let a;
+$('.right').on('click',change);
+$('.left').on('click', function () {change(1)});
 
-	$('#button').children().on('click', function () {
-		if ($('#day').prop("checked")) {
-			$('.active').children('.day').stop().fadeIn(2000, function () {
-				$('.active').children('.night').hide()
-			})
-		}
-		else {
-			$('.active').children('.night').show();
-			$('.active').children('.day').stop().fadeOut(2000)
-		}
-	});
 
-	function change() {
-		if ($('.active').next().length) {
-			if ($('#day').prop("checked")) {
-				$('.active').next().find('.night').hide();
-				$('.active').next().find('.day').show()
-			}
-			else {
-				$('.active').next().find('.day').hide();
-				$('.active').next().find('.night').show()
-			}
-			$('.active').removeClass('active').next().addClass('active');
-			$('.active').stop().fadeIn(2000, function () {
-				$('#slider').children().not('.active').hide();
-			});
-		} else {
-			if ($('#day').prop("checked")) {
-				$('.active').parent().children(':first').find('.night').hide();
-				$('.active').parent().children(':first').find('.day').show()
-			}
-			else {
-				$('.active').parent().children(':first').find('.day').hide();
-				$('.active').parent().children(':first').find('.night').show()
-			}
-			$('.active').removeClass('active').parent().children(':first').addClass('active');
-			$('.active').stop().fadeIn(2000, function () {
-				$('#slider').children().next(':last').hide();
-			});
-		}
+function change(a) {
+	if(a){index--}else {index++}
+	if(index>=$('#slider').children().length){index=0}
+	if (index<0){index=$('#slider').children().length-1}
+	$('#slider li').removeClass('active').eq(index).addClass('active');
 
+}
+$('#button').children().on('click', function () {
+	if ($('#day').prop("checked")) {
+		$('.day').addClass('active');
+		$('.night').removeClass('active');
 	}
+	else {
+		$('.day').removeClass('active');
+		$('.night').addClass('active');
+	}
+});
+
 
 
