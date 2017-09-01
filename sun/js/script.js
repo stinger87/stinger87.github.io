5ddd4bf6c63587b00d351cf3d176f624a7896198
Liber <stinger87@yandex.ru>
2017-09-01 19:27:26 +0300

    Add files via upload

--- sun/js/script.js
+++ sun/js/script.js
@@ -1,28 +1,57 @@
-setInterval(change,10000);
+let $length = $('#slider').children().length;
+let interval = setInterval(change,5000);
 let index=0;
+circle();
+$('.round:first').addClass('point');
 
-$('.right').on('click',change);
-$('.left').on('click', function () {change(1)});
+$('.right').on('click', function () {
+	change();
+	clearTimeout(interval);
+	interval = setInterval(change,5000);
+});
+$('.left').on('click', function () {
+	change({side:true});
+	clearTimeout(interval);
+	interval = setInterval(change,5000);
+});
+$('.round').on('click', function () {
+	change({number:$(this).index()+1});
+	clearTimeout(interval);
+	interval = setInterval(change,5000);
+});
 
-
-function change(a) {
-	let $length = $('#slider').children().length;
-	if(a){index--}else {index++}
-	if(index>=$length) {index = 0}
-	if(index<0) {index = $length - 1}
+function change(options) {
+	options=$.extend({
+		number:false,
+		side:false
+	},options);
+	if (options.side){
+		index--
+	} else {
+		index++
+	}
+	if (index>=$length) { index = 0}
+	if (index<0) {index = $length - 1}
+	if (options.number) {index = options.number-1}
 	$('#slider li').removeClass('active').eq(index).addClass('active');
-
+	$('.circle').children().removeClass('point').eq($('#slider').children('.active').index()).addClass('point');
 }
 $('#button').children().on('click', function () {
 	if ($('#day').prop("checked")) {
 		$('.day').addClass('active');
 		$('.night').removeClass('active');
-	}
-	else {
+	}	else {
 		$('.day').removeClass('active');
 		$('.night').addClass('active');
 	}
 });
+function circle() {
+	for (let i=0;i<$length;i++) {
+		$('.circle').append('<span class="round"></span>')
+	}
+}
+
+
 
 
 
