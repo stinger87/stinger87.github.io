b25f3cecdc6778a85a91ddb7d7ef7df4b383e2a1
Liber <stinger87@yandex.ru>
2017-09-08 12:31:49 +0300

    Add files via upload

--- sun/js/script.js
+++ sun/js/script.js
@@ -1,48 +1,23 @@
 $( document ).ready(function() {
 	let $length = $('#slider').children().length;
-	let interval = setInterval(change, 5000);
+	let interval;
 	let index = 0;
+	inter();
 	circle();
 	$('.round:first').addClass('point');
 
 	$('.right').on('click', function () {
-		change();
-		clearTimeout(interval);
-		interval = setInterval(change, 5000);
+		side(0);
+		inter();
 	});
 	$('.left').on('click', function () {
-		change({side: true});
-		clearTimeout(interval);
-		interval = setInterval(change, 5000);
+		side(1);
+		inter();
 	});
 	$('.round').on('click', function () {
-		change({number: $(this).index() + 1});
-		clearTimeout(interval);
-		interval = setInterval(change, 5000);
+		number($(this).index());
+		inter();
 	});
-
-	function change(options) {
-		options = $.extend({
-			number: false,
-			side: false
-		}, options);
-		if (options.side) {
-			index--
-		} else {
-			index++
-		}
-		if (index >= $length) {
-			index = 0
-		}
-		if (index < 0) {
-			index = $length - 1
-		}
-		if (options.number) {
-			index = options.number - 1
-		}
-		$('#slider li').removeClass('active').eq(index).addClass('active');
-		$('.circle').children().removeClass('point').eq($('#slider').children('.active').index()).addClass('point');
-	}
 
 	$('#button').children().on('click', function () {
 		if ($('#day').prop("checked")) {
@@ -53,6 +28,37 @@ $( document ).ready(function() {
 			$('.night').addClass('active');
 		}
 	});
+
+	function change() {
+
+		if (index >= $length) {
+			index = 0
+		}
+		if (index < 0) {
+			index = $length - 1
+		}
+		$('#slider li').removeClass('active').eq(index).addClass('active');
+		$('.circle').children().removeClass('point').eq(index).addClass('point');
+	}
+	function side(left) {
+		if (left) {
+			index--
+		} else {
+			index++
+		}
+		$('#slider li').removeClass('active').eq(index).addClass('active');
+		$('.circle').children().removeClass('point').eq(index).addClass('point');
+	}
+
+	function number(i) {
+		$('#slider li').removeClass('active').eq(i).addClass('active');
+		$('.circle').children().removeClass('point').eq(i).addClass('point');
+	}
+
+	function inter() {
+		clearTimeout(interval);
+		interval = setInterval(change, 5000);
+	}
 
 	function circle() {
 		for (let i = 0; i < $length; i++) {
